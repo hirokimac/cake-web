622a0ef1c361bfb0d6bc2cb8c1d95924515ab484
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-04-12 23:36:14 +0900

    20210412_add_routes_cake_web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    member do
+      get :quit
+      delete :withdrawal
+      get :withdrawal_updata
+
+      resources :carts, only: [:create, :index, :update, :destroy] do
+        collection do
+          delete :destroy_all
+        end
+      end
+    
+      resources :orders, only: [:new, :create, :index, :show] do
+        collection do
+          get :order_check
+          get :thanks
+        end
+        member do
+          resource :order_items, only: [:create]
+        end
+      end
+
+      resources :delivery_address, only: [:index, :create, :edit, :update, :destroy]
+
+      get 'cart_items' => 'cart_items#destroy_all'
+      
+    end
+    
+  end
+
+  resources :products, only: [:show, :index] 
+
+  get 'about' => 'homes#about'
+  
+  root 'homes#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
