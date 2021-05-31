7be896c2668fd5b65a6a7a6ff10fc475b01817b6
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-05-31 22:13:29 +0900

    20210531_change_carts_create

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,7 @@ class CartsController < ApplicationController
 
     def create
         @cart = current_user.carts.new(cart_params)
+        
     # カートの中に同じ商品が重複しないようにして　古い商品と新しい商品の数量を合わせる
         @update_cart =  Cart.find_by(product: @cart.product)
     if  @update_cart.present? && @cart.valid?
@@ -14,15 +15,22 @@ class CartsController < ApplicationController
     if  @cart.save
         redirect_to carts_path
     else
+        @product = Product.find(params[:cart][:product_id])
+        @cart = Cart.new
         render ("products/show")
     end
     end
 
     def index
+        @user = current_user
         @carts = current_user.carts
-    end
+        end
 
     def update
+        @user = current_user
+        @cart = Cart.find(params[:id])
+        @cart.update(cart_params)
+        redirect_to cart_path
     end
 
     def destroy
@@ -33,11 +41,14 @@ class CartsController < ApplicationController
     end
 
     def destroy_all
+        cart = current_user.carts
+        cart.destroy
+        redirect_to cart_path(current_user.id)
     end
 
     private
-    def cart_params
-        params.require(:cart).permit(:user_id, :product_id, :quantity,)
-    end
-
+        def cart_params
+            params.require(:cart).permit(:user_id, :product_id, :quantity )
+        end    
+    
 end
